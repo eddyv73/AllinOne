6916e4a300eaa7524cdcb0718e3b61db1a4827db
Wister <e@wister.mx>
2020-01-12 20:54:26 -0600

    add kubectl
    AKS and Deployment

---
+++ aks.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+##Prev az login##
+##Only for get context in aks##
+echo Get  AKS Context
+az aks get-credentials --resource-group aks --name wister
+kubectl create namespace pyproject
+##Only if hub is private##
+##kubectl -n pyproject create secret docker-registry wistersecret --docker-server=URLDOCKERHUB --docker-username=wister --docker-password=PWDHUB --docker-email=e@wister.mx
+
+echo Deployment begin
+kubectl -n pyproject apply -f ./deploymentpyproject
+echo Deployment End
+
+kubectl -n pyproject get pods
+
